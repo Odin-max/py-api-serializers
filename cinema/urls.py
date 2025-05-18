fabaf4eff5994e16016659e1499ce672557861a3
Odin-max <meh84741@gmail.com>
2025-05-18 18:49:39 +0300

    1

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,17 @@
-# write urls here
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import (
+    GenreViewSet, ActorViewSet, CinemaHallViewSet,
+    MovieViewSet, MovieSessionViewSet
+)
+
+router = DefaultRouter()
+router.register(r"cinema/genres", GenreViewSet)
+router.register(r"cinema/actors", ActorViewSet)
+router.register(r"cinema/cinema_halls", CinemaHallViewSet)
+router.register(r"cinema/movies", MovieViewSet)
+router.register(r"cinema/movie_sessions", MovieSessionViewSet)
+
+urlpatterns = [
+    path("api/", include(router.urls)),
+]
